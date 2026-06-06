19305b1698f90dffcbc8adc23545b5a47e2502b9
Charlotte Wang <wangyicharlotte@hotmail.com>
2026-06-06 11:01:10 -0700

    Add some info

---
+++ app/work/page.tsx
@@ -0,0 +1,16 @@
+export default function WorkPage() {
+    return (
+        <section>
+            <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+                Work
+            </h1>
+            <div className="prose">
+                <h2>My Work</h2>
+                <p className="text-nerutral-600 dark:text-neutral-400">
+                    I am an AI native machine learning engineer. I am building my workflow on top of AI tools to improve efficiency and creativity. I am passionate about using AI to solve real-world problems and create value for users.
+                </p>
+                <p>I would try to put what I have done and learn here</p>
+            </div>
+        </section>
+    )
+}
